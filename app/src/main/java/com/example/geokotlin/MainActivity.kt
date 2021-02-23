2c600e91e253ea927554b46f9aef064cc388d50f
David <dvaquero@bilbomatica.es>
2021-02-23 19:38:51 +0100

    metidos ejemplos de watch y clearwatch

--- app/src/main/java/com/example/geokotlin/MainActivity.kt
+++ app/src/main/java/com/example/geokotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geokotlin
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -13,17 +14,51 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+
 
 class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
     private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private var locationCallback: LocationCallback? = null
+    private var mLocationRequest: LocationRequest? = null
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+
     private var texto: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         texto = findViewById(R.id.texto)
+        mLocationRequest = LocationRequest()
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest?.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest?.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest?.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    Log.d("app", "location: " + location)
+                }
+            }
+        }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -38,6 +73,14 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedList
         val id = item.itemId
         if (id == R.id.action_get_location) {
             requestLocationPermissions()
+            return true
+        }
+        if (id == R.id.action_watch_location) {
+            watchPosition()
+            return true
+        }
+        if (id == R.id.action_clear_watch_location) {
+            clearWatchPosition()
             return true
         }
         return super.onOptionsItemSelected(item)
@@ -93,6 +136,24 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedList
         }
     }
 
+    fun watchPosition(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        mFusedLocationClient?.requestLocationUpdates(mLocationRequest,
+                locationCallback,
+                Looper.getMainLooper())
+    }
+    fun clearWatchPosition(){
+        mFusedLocationClient?.removeLocationUpdates(locationCallback)
+    }
 
 
     private fun requestPosition() {
